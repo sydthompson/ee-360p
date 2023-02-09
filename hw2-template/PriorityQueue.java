707352cca328155184e45ec6c3e562846528809f
sydthompson <sydneythompson@utexas.edu>
2023-02-08 19:42:48 -0600

    Adding eids to files. Setting up P3.

--- hw2-template/PriorityQueue.java
+++ hw2-template/PriorityQueue.java
@@ -1,6 +1,6 @@
 
-// EID 1
-// EID 2
+// st34596
+// mhv352
 
 public class PriorityQueue {
 
@@ -13,16 +13,19 @@ public class PriorityQueue {
         // Returns the current position in the list where the name was inserted;
         // otherwise, returns -1 if the name is already present in the list.
         // This method blocks when the list is full.
+                return 0;
 	}
 
 	public int search(String name) {
         // Returns the position of the name in the list;
         // otherwise, returns -1 if the name is not found.
+                return 0;
 	}
 
 	public String getFirst() {
         // Retrieves and removes the name with the highest priority in the list,
         // or blocks the thread if the list is empty.
+                return "";
 	}
 }
 
