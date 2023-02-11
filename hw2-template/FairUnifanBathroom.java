afbb2a158d3b45d243f564e8148b1984869456c1
mvu <54677355+mhvu2023@users.noreply.github.com>
2023-02-11 00:29:21 -0600

    Updated HW2Q2 soln

--- hw2-template/FairUnifanBathroom.java
+++ hw2-template/FairUnifanBathroom.java
@@ -3,33 +3,48 @@
 
 import java.util.concurrent.locks.*;
 
+
 public class FairUnifanBathroom {
 
-	private int ticketNumber = 0;
+	private int ticketNumber = 0, ticketToEnter = 1;
 	private int occupancy = 0;
 	private final int capacity = 7;
 
+	enum College {
+		UT,
+		OU,
+		NONE
+	}
+
+	private College occupier = College.NONE;
+
 	private final ReentrantLock bathroomLock = new ReentrantLock(true);
 
-							// If awaited, UT is in the bathroom
-	private final Condition notUT = bathroomLock.newCondition(),
-							// If awaited, OU is in the bathroom
-							notOU = bathroomLock.newCondition();
-
 							// Lock out ALL fans when the bathroom is full
-	private final Condition notFull = bathroomLock.newCondition();
+	private final Condition notFull = bathroomLock.newCondition(),
+							notMyTurn = bathroomLock.newCondition();
 
 	public void enterBathroomUT() throws InterruptedException{
 		bathroomLock.lock();
 		try {
-			// Lock the bathroom for UT if you're the first to arrive
-			if (occupancy == 0) notUT.await();
+			int myTicket = ticketNumber++;
 
-			occupancy++;
-			if (occupancy == capacity) {
-				// Wait until notFull is signaled by a leave
+			// Bathroom might already be full
+			while (occupancy == capacity) {
+				// Wait until someone signals a leave
 				notFull.await();
 			}
+
+			while ((occupier == College.OU) || (ticketToEnter != myTicket)) {
+				// Wait if you are not next in line
+				// OR OU is in the bathroom right now
+				notMyTurn.await();
+			}
+
+			// Bathroom is now UT's if everyone has exited when this fan enters
+			if (occupancy == 0) occupier = College.UT;
+			occupancy++;
+
 		} finally {
 			bathroomLock.unlock();
 		}
@@ -38,14 +53,23 @@ public class FairUnifanBathroom {
 	public void enterBathroomOU() throws InterruptedException{
 		bathroomLock.lock();
 		try {
-			//Lock the bathroom for OU if you're the first to arrive
-			if (occupancy == 0) notOU.await();
+			int myTicket = ticketNumber++;
 
-			occupancy++;
-			if (occupancy == capacity) {
-				// Wait until notFull is signaled by a leave
+			// Bathroom might already be full
+			while (occupancy == capacity) {
+				// Wait until someone signals a leave
 				notFull.await();
 			}
+
+			while ((occupier == College.UT) || (ticketToEnter != myTicket)) {
+				// Wait if you are not next in line
+				// OR UT is in the bathroom right now
+				notMyTurn.await();
+			}
+
+			if (occupancy == 0) occupier = College.OU;
+			occupancy++;
+
 		} finally {
 			bathroomLock.unlock();
 		}
@@ -56,13 +80,14 @@ public class FairUnifanBathroom {
 		try {
 			occupancy--;
 			// Let line know the bathroom is not at capacity
-			if (occupancy != capacity) {
-				notFull.signalAll();
-			}
-			// Let line know that UT fans have exited the bathroom
-			if (occupancy == 0) {
-				notUT.signalAll();
-			}
+			if (occupancy != capacity) notFull.signalAll();
+			// Just make sure the college is assigned correctly and that the next ticket to enter is updated
+			// Then notify all threads and they will check their ticket number again
+			if (occupancy == 0) occupier = College.NONE;
+
+			ticketToEnter++;
+			notMyTurn.notifyAll();
+
 		} finally {
 			bathroomLock.unlock();
 		}
@@ -73,16 +98,21 @@ public class FairUnifanBathroom {
 		try {
 			occupancy--;
 			// Let line know the bathroom is not at capacity
-			if (occupancy != capacity) {
-				notFull.signalAll();
-			}
-			// Let line know that OU fans have exited the bathroom
-			if (occupancy == 0) {
-				notOU.signalAll();
-			}
+			if (occupancy != capacity) notFull.signalAll();
+			// Just make sure the college is assigned correctly and that the next ticket to enter is updated
+			// Then notify all threads and they will check their ticket number again
+			if (occupancy == 0) occupier = College.NONE;
+
+			ticketToEnter++;
+			notMyTurn.notifyAll();
+
 		} finally {
 			bathroomLock.unlock();
 		}
+	}
+
+	public synchronized String bathroomStatus() throws InterruptedException{
+
 	}
 }
 	
